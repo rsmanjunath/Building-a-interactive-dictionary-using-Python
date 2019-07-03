0e94d6642826aa3d4bbd2977b87f722502e6bdfb
Manjunath <rsmanjunath97@gmail.com>
2019-07-03 12:40:53 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,18 @@
+import json
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
